66f1544605423100c3f50ab4dd1b315ea7d0ac7f
Andrew <chenpoanandrew@gmail.com>
2017-02-01 00:07:06 +0800

    get dir size on deploy

--- deploy2ftp.js
+++ deploy2ftp.js
@@ -2,6 +2,7 @@
 var fs = require('fs-extra');
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
+var getSize = require('get-folder-size');
 
 var config = {
 	host: "211.72.207.202", // www.andrewchen.tw
@@ -13,16 +14,25 @@ var config = {
 	// exclude: ['.git', '.idea', 'tmp/*']
 };
 
-ftpDeploy.deploy(config, function(err) {
-	var exec = require('child_process').exec;
-	var cmd = 'osascript -e \'display notification "@content" with title "@title"\'';
-	if (err) {
-		cmd = cmd.replace(/@title/, 'AndrewChenBlog ERROR').replace(/@content/, '上傳至FTP出現錯誤:' + err.message);
-		console.log('%j', err);
-	}
-	else {
-		cmd = cmd.replace(/@title/, 'AndrewChenBlog').replace(/@content/, '上傳至FTP完成');
-		console.log('FINISHED');
-	}
-	exec(cmd);
+// get size
+getSize(__dirname + "/public", function(err, size) {
+  if (err) { throw err; }
+  console.log('public dir size : ' + size + ' bytes');
+  console.log('public dir size : ' + (size / 1024 / 1024).toFixed(2) + ' Mb');
+
+	// start deploy
+	ftpDeploy.deploy(config, function(err) {
+		var exec = require('child_process').exec;
+		var cmd = 'osascript -e \'display notification "@content" with title "@title"\'';
+		if (err) {
+			cmd = cmd.replace(/@title/, 'AndrewChenBlog ERROR').replace(/@content/, '上傳至FTP出現錯誤:' + err.message);
+			console.log('%j', err);
+		}
+		else {
+			cmd = cmd.replace(/@title/, 'AndrewChenBlog').replace(/@content/, '上傳至FTP完成');
+			console.log('FINISHED');
+		}
+		exec(cmd);
+	});
+
 });
