649839900f0982f5bf00a11ec5183bfd421649ba
AndrewChen <chenpoanandrew@gmail.com>
2017-04-21 13:13:59 +0800

    all minify

--- public/scripts/icon_fa-arrow-right.js
+++ public/scripts/icon_fa-arrow-right.js
@@ -1,8 +1,1 @@
-// 將 -> 更換成 i.fa.fa-arrow-right
-
-var article = $('#main').html();
-var scriptStart = article.indexOf('<script>');
-article = article.substring(0, scriptStart); // 移除這串 script
-article = article.replace(/-&gt;/g, '<i class="fa fa-arrow-right" aria-hidden="true"></i>');
-$('#main').html(article);
-</script>
+var article=$("#main").html();var scriptStart=article.indexOf("<script>");article=article.substring(0,scriptStart);article=article.replace(/-&gt;/g,'<i class="fa fa-arrow-right" aria-hidden="true"></i>');$("#main").html(article);</script>
